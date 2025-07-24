25cb415e003f14579f20e4e1da7b331ed1e16a97
elliott <94141340+cmnowastaken@users.noreply.github.com>
2025-07-24 15:19:11 +1200

    Managed to create a GUI for the buttons
    Working on adding action listeners so that the buttons actually do something, will work on prettiness later

--- DashboardMain.java
+++ DashboardMain.java
@@ -1,11 +1,11 @@
 /* 
  * Theme Park Dashboard
  * 
- * Implementing the very basic menu
+ * Implementing a GUI menu
  * 
  * Elliott Bell
  * 
- * 26/06/25
+ * 24/07/25
 */
 
 import javax.swing.*;
@@ -15,18 +15,12 @@ import java.awt.Dimension;
 import java.awt.event.*;
 
 public class DashboardMain extends JFrame implements ActionListener {
-    
-    JMenuBar mainMenuBar;
-    JMenu mainMenu;
-    JMenuItem mainMenuItem;
-    JButton Corkscrew;
-    
+    JFrame mainWindow;
     String windowName;
     
     private ArrayList<JButton> rideButtons;
     
-    public DashboardMain() {        
-        String[] rideNames = {"Corkscrew",
+    String[] rideNames = {"Corkscrew",
                           "Bumper Cars",
                           "Go Karts",
                           "The Viggler's Fun Wheel",
@@ -34,9 +28,48 @@ public class DashboardMain extends JFrame implements ActionListener {
                           "The Long Drop",
                           "Doom Flume",
                           "Hades' Inferno"};
-                        
-        rideButtons = new ArrayList<>();
+    
+    public DashboardMain() {        
+        mainWindow = new JFrame("The Viggler's Fun Emporium");
         
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        
+        rideButtons = new ArrayList<>();
+
+        for (String rideName : rideNames) {
+            JButton rideButton = new JButton(rideName);
+            rideButtons.add(rideButton);
+        }
+        
+        for (JButton ride : rideButtons) {
+            ride.setIcon(new ImageIcon("Icons/Corkscrew_Art.png"));
+            ride.setHorizontalTextPosition(JButton.CENTER);
+            ride.setVerticalTextPosition(JButton.BOTTOM);
+            ride.setFocusable(false);
+            panel.add(ride);
+        }
+    
+        mainWindow.add(panel);
+        mainWindow.setSize(1000, 1000);
+        mainWindow.setVisible(true);
+        
+        /* Corkscrew = new JButton();
+        Corkscrew.setIcon(new ImageIcon("Icons/Corkscrew_Art.png"));
+        Corkscrew.setBounds(40, 40, 160, 160);
+        Corkscrew.setFocusable(false);
+        Corkscrew.addActionListener(this);
+        this.add(Corkscrew); 
+
+        setTitle("The Viggler's Fun Emporium"); // set the window name for the user input
+        this.getContentPane().setPreferredSize(new Dimension(600, 400)); // set the dimensions of the window off the user inputs
+        this.getContentPane().setLayout(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE); // close the window when the program closes.
+        
+        this.pack(); // makes contents above preferred size
+        this.toFront(); // states that the window should be at the front
+        this.setVisible(true); // makes the window visible
+                                
         mainMenuBar = new JMenuBar();
         this.setJMenuBar(mainMenuBar);
         
@@ -51,56 +84,17 @@ public class DashboardMain extends JFrame implements ActionListener {
         
         mainMenuItem = new JMenuItem("Staff");
         mainMenu.add(mainMenuItem);
-        
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
-
-        setTitle("The Viggler's Fun Emporium"); // set the window name for the user input
-        this.getContentPane().setPreferredSize(new Dimension(600, 400)); // set the dimensions of the window off the user inputs
-        this.getContentPane().setLayout(null);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE); // close the window when the program closes.
-        
-        this.pack(); // makes contents above preferred size
-        this.toFront(); // states that the window should be at the front
-        this.setVisible(true); // makes the window visible
-        
-        for (String rideName : rideNames) {
-            JButton rideButton = new JButton(rideName);
-            rideButtons.add(rideButton);
-        }
-        
-        for (JButton ride : rideButtons) {
-            ride.setIcon(new ImageIcon("Icons/Corkscrew_Art.png"));
-            ride.setBounds(40, 40, 160, 160);
-            ride.setFocusable(false);
-            panel.add(ride);
-        }
-        
-        /* Corkscrew = new JButton();
-        Corkscrew.setIcon(new ImageIcon("Icons/Corkscrew_Art.png"));
-        Corkscrew.setBounds(40, 40, 160, 160);
-        Corkscrew.setFocusable(false);
-        Corkscrew.addActionListener(this);
-        this.add(Corkscrew); 
         */
     }
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == Corkscrew) {
-            System.out.println("What the freak");
-        // } else if (e.getSource() == BumperCars) {
-            
-        } else if (e.getSource() == Corkscrew) {
-            
-        } else if (e.getSource() == Corkscrew) {
-            
-        } else if (e.getSource() == Corkscrew) {
-            
-        } else if (e.getSource() == Corkscrew) {
-            
-        } else if (e.getSource() == Corkscrew) {
-            
-        } else if (e.getSource() == Corkscrew) {
-            
+        Object buttonClicked = e.getSource();
+        
+        for (int i = 0; i < rideButtons.size(); i++) {
+            if (buttonClicked == rideButtons.get(i)) {
+                System.out.println(rideNames[i]);
+                
+            }
         }
     }
+    // private void RideHandler()
 }
